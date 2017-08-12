7678ce8661f51298cd534859a97c42606bb402f2
Eric Hosford <eric.hosford@gmail.com>
2017-08-11 20:18:35 -0400

    Added search capability

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyAKnLK3Dmw3c7XS9esw9a1-ZzI8UxbTuSc';
 
-// Create a new component. This component should produces some HTML
-const App = () => {
-  return ( <div>
-    <SearchBar / >
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] }
+
+    YTSearch({key: API_KEY, term: 'Hot'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return ( 
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
-
-// Take this components generated HTML and put it on the page (in the DOM)
 
 ReactDOM.render( < App / > , document.querySelector('.container'));
